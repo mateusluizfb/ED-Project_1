99b0bd0b93853e8f10850dd219099645fb213d64
Mateus Luiz <mateuslfb@hotmail.com>
2016-04-18 13:35:00 -0300

    SelectionSort implemented with LinkedList

---
+++ ED-Projeto_1/src-tests-projeto/Sort/testSelectionSort.java
@@ -0,0 +1,46 @@
+package Sort;
+
+import org.junit.Before;
+
+import Lists.LinkedList;
+import Lists.List;
+import junit.framework.TestCase;
+
+public class testSelectionSort extends TestCase {
+	
+	List array = new LinkedList();
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		array.insert(0, 10);
+		array.insert(1, 9);
+		array.insert(2, 8);
+		array.insert(3, 7);
+	
+	}
+	
+	public void testInsert(){
+		
+		assertEquals(10, array.elementAt(0));
+		assertEquals(9, array.elementAt(1));
+		assertEquals(8, array.elementAt(2));
+		assertEquals(7, array.elementAt(3));
+		
+	}
+	
+	public void testSort(){
+		
+		SelectionSort ss = new SelectionSort();
+		
+		ss.sort(array);
+		
+		assertEquals(7, array.elementAt(0));
+		assertEquals(8, array.elementAt(1));
+		assertEquals(9, array.elementAt(2));
+		assertEquals(10, array.elementAt(3));
+		
+		
+	}
+
+}
